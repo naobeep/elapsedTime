2bbd13fa2714e0eac6f4ba8e13dc190c03b30ed9
naobeep <85398103+naobeep@users.noreply.github.com>
2024-02-20 08:29:46 +0900

    202402020

---
+++ elapsedTime.js
@@ -0,0 +1,10 @@
+export const elapsedTime = async func => {
+  console.log('Start!'.warn);
+  const startTime = new Date();
+
+  await func();
+
+  console.log('Complete!'.warn);
+  const elapsedTime = new Date() - startTime;
+  console.log(`Execution time: ${elapsedTime}ms`.verbose);
+};
